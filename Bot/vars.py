71a25a390a8c8560d1428939687beb106d6d7ef4
DeekshithSH <shdeekshith@gmail.com>
2023-07-26 12:26:33 +0530

    just bot

--- Bot/vars.py
+++ Bot/vars.py
@@ -16,15 +16,6 @@ class Var(object):
    ENDMSGID=environ.get("ENDMSGID", None)
    ENDMSGID=int(ENDMSGID) if ENDMSGID else int(STARTMSGID+200)
 
-   PORT = int(environ.get("PORT", 8080))
-   BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
-   HAS_SSL = environ.get("HAS_SSL", False)
-   HAS_SSL = True if str(HAS_SSL).lower() == "true" else False
-   NO_PORT = environ.get("NO_PORT", False)
-   NO_PORT = True if str(NO_PORT).lower() == "true" else False
-   FQDN = str(environ.get("FQDN", BIND_ADDRESS))
-   URL = "http{}://{}{}/".format(
-            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT))
    OWNER_ID=list(set(int(x) for x in str(environ.get("OWNER_ID", "849816969")).split()))
    ADMIN_ID=list(set(int(x) for x in str(environ.get("ADMIN_ID", "849816969")).split()))
    ADMIN_ID.extend(OWNER_ID)
--- runbot.py
+++ runbot.py
@@ -3,10 +3,7 @@ import sys
 import logging
 import logging.handlers as handlers
 from pyrogram import idle
-from aiohttp import web
-from Bot.vars import Var
 from Bot.bot import TGBot
-from Bot.server import web_server
 from Bot.utils.update import update_file, fix_name
 
 logging.basicConfig(
@@ -14,13 +11,9 @@ logging.basicConfig(
     datefmt="%d/%m/%Y %H:%M:%S",
     format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler(stream=sys.stdout),
-              handlers.RotatingFileHandler("streambot.log", mode="a", maxBytes=104857600, backupCount=2, encoding="utf-8")],)
+              handlers.RotatingFileHandler("bot.log", mode="a", maxBytes=104857600, backupCount=2, encoding="utf-8")],)
 
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
-
-server = web.AppRunner(web_server())
 
 loop = asyncio.get_event_loop()
 
@@ -29,14 +22,6 @@ async def main():
     await TGBot.start()
     # await update_file()
     # await fix_name()
-
-    if True:
-        logging.info("Initalizing Web Server")
-        await server.setup()
-        bind_address = Var.BIND_ADDRESS
-        await web.TCPSite(server, bind_address, Var.PORT).start()
-        logging.info("DONE")
-        logging.info(f"API running on {Var.URL}")
 
     await idle()
 if __name__ == "__main__":
